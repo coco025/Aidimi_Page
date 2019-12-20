e3c5e274b165bda4d84559ddce0df7ee92d92f9f
coco025 <luiscocose025@gmail.com>
2019-12-19 23:25:09 -0600

    Push

--- docs/assets/js/navBar.js
+++ docs/assets/js/navBar.js
@@ -1,3 +1,16 @@
 document.getElementById("following").setAttribute("onclick","window.location.href = 'main_page.html';");
 
 document.getElementById("search_purposes").setAttribute("onclick","window.location.href = 'main_page.html';");
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navBar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
