45d5670f7339a4a342b444d34510c95b91b34146
coco025 <luiscocose025@gmail.com>
2020-01-21 22:19:35 -0600

    Push

--- docs/assets/js/sleep.js
+++ docs/assets/js/sleep.js
@@ -4,7 +4,12 @@ function sleep_aux(ms) {
 
 async function sleep(ms) {
     await sleep_aux(ms);
+    var element = document.getElementById("img_center"); 
+    document.getElementById("body_start_page").style.backgroundColor = "#dddddd";
+    element.classList.add("zoomOutDown");
+    
+        await sleep_aux(1000);
     window.location.replace("login.html");
 }
 
-//document.getElementById("body_start_page").setAttribute("onload", "sleep(3000);");
+document.getElementById("body_start_page").setAttribute("onload", "sleep(3000);");
