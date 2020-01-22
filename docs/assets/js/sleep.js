b06ffde697245cb6ea0c93234b61b07d8864b2fe
coco025 <luiscocose025@gmail.com>
2020-01-21 23:38:23 -0600

    Push

--- docs/assets/js/sleep.js
+++ docs/assets/js/sleep.js
@@ -16,8 +16,8 @@ async function setFavorite(ms) {
 async function indexPage(ms){
     await sleep(ms);
     var element = document.getElementById("img_center"); 
-    //element.classList.add("zoomOutDown");
-    var element = document.getElementById("loading"); 
+    element.classList.add("fadeOut");
+    element = document.getElementById("loading"); 
     element.classList.add("zoomOutDown");
     await sleep(1000);
     window.location.replace("login.html");
