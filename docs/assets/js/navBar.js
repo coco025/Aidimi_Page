a146803485ded73493d7b2d276ce30ca2d2c9f6a
coco025 <luiscocose025@gmail.com>
2020-02-01 21:01:21 -0600

    Push

--- docs/assets/js/navBar.js
+++ docs/assets/js/navBar.js
@@ -1,6 +1,6 @@
 document.getElementById("following").setAttribute("onclick","window.location.href = 'main_page.html';");
 
-document.getElementById("search_purposes").setAttribute("onclick","window.location.href = 'main_page.html';");
+document.getElementById("search_purposes").setAttribute("onclick","window.location.href = 'explore.html';");
 
 window.onscroll = function() {myFunction()};
 
