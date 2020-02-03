bf782f22b4d51f3581dec0b3e73e8265480b6f3f
coco025 <luiscocose025@gmail.com>
2020-02-02 18:45:41 -0600

    Push

--- docs/assets/js/qr_reader.js
+++ docs/assets/js/qr_reader.js
@@ -1,10 +1,10 @@
-async function Qrwait(){
-    alert("s");   document.getElementById("main_qrReader").style.opacity = 0;
+async function Qrwait(ms){
+    //alert("s");   document.getElementById("body_reader").style.opacity = 0;
 
-    await sleep(2000);
+    await sleep(ms);
     
-    document.getElementById("main_qrReader").style.opacity = 1;
+    document.getElementById("body_reader").style.opacity = 1;
 
 }
 
-document.getElementById("main_qrReader").setAttribute("onclick","Qrwait()");
+document.getElementById("body_reader").setAttribute("onload","Qrwait(2000);");
