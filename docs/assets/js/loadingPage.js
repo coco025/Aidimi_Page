3c8a15989c22ba96c66c11204b282601dd2446b7
coco025 <luiscocose025@gmail.com>
2020-05-11 23:48:01 -0600

    Push

--- docs/assets/js/loadingPage.js
+++ docs/assets/js/loadingPage.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('#body').show();
     $('#loading').hide();
+});*/
+$('#body').css('opacity', 0);
+$(window).load(function() {
+  $('#body').css('opacity', 1);
 });
-
