4084b275ae3e50186efa3be20620079e11abdab1
coco025 <luiscocose025@gmail.com>
2020-05-11 23:57:10 -0600

    Push

--- docs/assets/js/flipCard.js
+++ docs/assets/js/flipCard.js
@@ -1,12 +1,5 @@
 var card = document.querySelector('.cardFlip');
-card.addEventListener( 'click', async function() {
-  card.classList.toggle('is-flipped');
-    
-    if(document.getElementById('generalInfo').style.display === 'none'){
-        document.getElementById('generalInfo').style.display = '';
-    }else{
-        await sleep(400);
-        document.getElementById('generalInfo').style.display = 'none';
-    }
+card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
 });
 
