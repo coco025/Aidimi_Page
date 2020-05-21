bd5f08842c4811a08c53326c1c11b9e58ad7a348
coco025 <31966704+coco025@users.noreply.github.com>
2020-05-20 21:42:35 -0600

    Update login.js

--- docs/assets/js/login.js
+++ docs/assets/js/login.js
@@ -2,17 +2,14 @@ var xhttp = new XMLHttpRequest();
 
 function login()
 {	
-	 var data = {
-    email: "a@a",
-    password: "abc"
-  };
-	var json = JSON.stringify(data);
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			//window.location.href = "mainPage_comunity.html";
 			console.log(xhttp.responseText);
 		}
 	};
+	
+xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhttp.open("post", "https://api-aidimi.herokuapp.com/auth/login", true);
-	xhttp.send(json);	
+	xhttp.send();	
 }
